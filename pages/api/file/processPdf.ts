c191c00a2c7b3e2d768c48670c9d7db9053169af
Augusto Romero <augustoromerodev@gmail.com>
2023-05-08 18:06:43 -0300

    Process the pdf and save it to vector database

--- pages/api/file/processPdf.ts
+++ pages/api/file/processPdf.ts
@@ -1,7 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-import { promises as fs } from "fs";
-import { IncomingForm } from "formidable";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { initPinecone } from "../../../utils/pinecone-client";
+import {
+  PINECONE_INDEX_NAME,
+  PINECONE_NAME_SPACE,
+} from "../../../config/pinecone";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,8 +22,29 @@ export default async function handler(
       const loader = new PDFLoader(fileBlob, {
         splitPages: false,
       });
-      const docs = await loader.load();
+      const rawDocs = await loader.load();
 
+      //Split the documents
+      const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 400,
+        chunkOverlap: 50,
+        separators: ["\n\n", "\n", " ", ""],
+      });
+
+      const docs = await splitter.splitDocuments(rawDocs);
+      console.log("split docs", docs);
+      console.log("creating vector store...");
+
+      /*create and store the embeddings in the vectorStore*/
+      const embeddings = new OpenAIEmbeddings();
+      const pinecone = await initPinecone();
+      console.log(PINECONE_INDEX_NAME);
+      const index = pinecone.Index(PINECONE_INDEX_NAME);
+      await PineconeStore.fromDocuments(docs, embeddings, {
+        pineconeIndex: index,
+        namespace: PINECONE_NAME_SPACE,
+        textKey: "text",
+      });
       res.status(200).json({ documents: docs });
     } catch (error) {
       res.status(500).json({ message: "Error processing PDF" });
