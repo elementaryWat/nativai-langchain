602656cba1c364a920fee63236ffb613fb45d34d
Augusto Romero <augustoromerodev@gmail.com>
2023-06-01 20:20:42 -0300

    Viewport fixed

--- components/styles.ts
+++ components/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ChatContainer = styled("div")`
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
 `;
 
 export const StyledMessagesContainer = styled("div")`
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
 import { Provider } from "react-redux";
 import { store } from "../store";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // when the page loads, set the variable
+    const setHeight = () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+    setHeight();
+
+    // when the window is resized, we update the value of --vh
+    window.addEventListener("resize", setHeight);
+
+    // remove the listener when the component is unmounted
+    return () => window.removeEventListener("resize", setHeight);
+  }, []);
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
