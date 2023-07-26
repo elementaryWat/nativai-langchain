dba85055c5af8aecdc20530c3b3723609063bb71
Augusto Romero <augustoromerodev@gmail.com>
2023-07-26 20:42:08 -0300

    secret fix google

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,28 +8,27 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      redirectUrl: "/login?callbackUrl=/",
     }),
-    // FacebookProvider({
-    //   clientId: "2634906513315037",
-    //   clientSecret: "4a0988e342b1cd2891636be78dbab749",
-    // }),
   ],
-  //   pages: {
-  //     signIn: "/auth/signin", //custom sign-in page
+  secret: process.env.SECRET,
+  pages: {
+    signIn: "/auth/signin", //custom sign-in page
+  },
+  // callbacks: {
+  //   async signIn(user, account, profile) {
+  //     if (account.provider === "google" && profile.verified_email === true)
+  //       return true;
+  //     else return false; // if you return false, access will be denied
   //   },
-  //   callbacks: {
-  //     async signIn(user, account, profile) {
-  //       if (account.provider === "google" && profile.verified_email === true)
-  //         return true;
-  //       else return false; // if you return false, access will be denied
-  //     },
-  //     async session(session, token) {
-  //       session.user.id = token.id;
-  //       return session;
-  //     },
-  //     async jwt(token, user) {
-  //       if (user) token.id = user.id;
-  //       return token;
-  //     },
+  //   async session(session, token) {
+  //     session.user.id = token.id;
+  //     console.log(session)
+  //     return session;
   //   },
+  //   async jwt(token, user) {
+  //     if (user) token.id = user.id;
+  //     return token;
+  //   },
+  // },
 });
