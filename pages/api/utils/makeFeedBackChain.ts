6d4ddc86e9f2dd307bbd21445f8f15abe69e87d2
Augusto Romero <augustoromerodev@gmail.com>
2023-07-28 13:04:27 -0300

    Mejora prompt feedback

--- pages/api/utils/makeFeedBackChain.ts
+++ pages/api/utils/makeFeedBackChain.ts
@@ -2,6 +2,7 @@ import { LLMChain } from "langchain/chains";
 
 import { PromptTemplate } from "langchain/prompts";
 import { OpenAI } from "langchain/llms/openai";
+import { AI_FEEDBACK_PROMPT } from "../../../utils/const";
 
 export const makeFeedbackChain = () => {
   const model = new OpenAI({
@@ -10,18 +11,7 @@ export const makeFeedbackChain = () => {
     topP: 1,
     timeout: 20000,
   });
-  const prompt = PromptTemplate.fromTemplate(
-    `As a fluent English speaker, evaluate my English proficiency in my response to a message. 
-    Given the messages delimited by ///:
-    Message:/// {message} ///.
-    and the response:/// {response} ///.
-    Offer precise suggestions for grammar and vocabulary improvement, supplemented with examples.
-    Don't be vague and enrich your feedback with examples for clearer understanding.
-    Assign a proficiency score from 0-5 for both grammar and vocabulary.
-    Your feedback should be in Spanish, but maintaining original English terms when referencing specific vocabulary or grammar points.
-    Provide the output in a JSON object with the following keys: grammar_feedback,grammar_score, vocabulary_score, vocabulary_feedback, general_score.   
-    `
-  );
+  const prompt = PromptTemplate.fromTemplate(AI_FEEDBACK_PROMPT);
   const chain = new LLMChain({ llm: model, prompt });
 
   return chain;
--- utils/const.ts
+++ utils/const.ts
@@ -3,6 +3,25 @@ export const SYSTEM_PROMPT =
   "You are Nati, a helpful AI assistant, who aids humans in practicing English by discussing {topic} at a {level} level. All your responses are in English, and if a user responds in a different language, you tactfully redirect them back to English.  Your responses are brief and concise, limited to a maximum of two sentences and designed to steer the conversation back to the topic at hand when the user get distracted. Additionally, your responses are designed to facilitate and extend the conversation. Begin by introducing yourself by your name, mentioning the topic for discussion, and offering an icebreaker";
 export const AI_INTRODUCTION_PROMPT =
   "Hello {username}, my name is Nati, and I'm here to talk about {topic} while practicing your english. To start, {intro}";
+// export const AI_FEEDBACK_PROMPT = `As a fluent English speaker, evaluate my English proficiency in my response to a message.
+//     Given the messages delimited by ///:
+//     Message:/// {message} ///.
+//     and the response:/// {response} ///.
+//     Offer precise suggestions for grammar and vocabulary improvement, supplemented with examples.
+//     Don't be vague and enrich your feedback with examples for clearer understanding.
+//     Assign a proficiency score from 0-5 for both grammar and vocabulary.
+//     Your feedback should be in Spanish, but maintaining original English terms when referencing specific vocabulary or grammar points.
+//     Provide the output in a JSON object with the following keys: grammar_feedback,grammar_score, vocabulary_score, vocabulary_feedback, general_score.
+//     `;
+export const AI_FEEDBACK_PROMPT = `
+1 - Ofrecer una retroalimentación integral y lo más específica posible sobre aspectos a mejorar en la gramática y el vocabulario a un mensaje de usuario delimitado por <<>> y sugerir mejoras también teniendo en cuenta el contexto de la conversación también delimitado por <<>>.
+2 - Asignar una puntuación de competencia de 0-5 tanto para gramática como para vocabulario.
+3 - Asegúrate de brindar la retroalimentación en español pero mantener los términos originales en inglés cuando se haga referencia a puntos específicos de vocabulario o gramática.
+4 - Tu respuesta debe ser un objeto json que contenga:  grammar_feedback,grammar_score, vocabulary_score, vocabulary_feedback, general_score. 
+    Contexto:<< {message} >>.
+    Message:<< {response} >>.
+`;
+
 export const RESPONSE_BACK_TO_ENGLISH =
   "Let's continue in English as it's the language we're using for our discussions";
 export const ENGLISH_CODE = "eng";
